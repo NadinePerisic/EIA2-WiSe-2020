e51e9b190c177e191b6d1348f24d112a3a67ad62
NadinePerisic <64103441+NadinePerisic@users.noreply.github.com>
2020-10-08 18:16:30 +0200

    Delete miniaufgabe.ts

--- miniaufgabe.ts
+++
@@ -1,7 +0,0 @@
-function myFunction() {
-    var person = prompt("Trage deinen Vornamen ein");
-    if (person != null) {
-      document.getElementById("popUp").innerHTML =
-      "Hallöchen   " + person + " wie geht es dir?  Bist du berreit für EIA2?";
-    }
-  }
