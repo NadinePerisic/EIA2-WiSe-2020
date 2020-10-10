2fa6d1ff02c9d440263217c80b9ede75afab44ff
NadinePerisic <64103441+NadinePerisic@users.noreply.github.com>
2020-10-10 20:52:08 +0200

    Delete miniaufgabe.ts

--- Aufgabe00/miniaufgabe.ts
+++
@@ -1,8 +0,0 @@
-window.addEventListener("load", myFunction);
-function myFunction() {
-    var person = prompt("Trage deinen Vornamen ein");
-    if (person != null) {
-      document.getElementById("popUp").innerHTML =
-      "Hallöchen   " + person + " wie geht es dir?  Bist du berreit für EIA2?";
-    }
-  }
